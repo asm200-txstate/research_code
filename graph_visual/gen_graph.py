31a102860a98cec00a71b43a3dc228a191c6bafd
axelsanchezmoreno <a_s1350@txstate.edu>
2025-05-27 23:24:25 -0500

    Added driver code and implemented generate_V

--- graph_visual/gen_graph.py
+++ graph_visual/gen_graph.py
@@ -40,12 +40,33 @@ class RandGraph:
     ## Name: generate_V
     ## Description: Generates the set of vertices, V, to the graph G = (V,E).
     ## Argument(s): None
-    def generate_V(self): pass
-    
+    def generate_V(self):
+        V = []                                              # Empty list of vertices to G = (V,E)
+        for v in range(self.card_v): V.append(f"v{v}")
+        return V
+            
     ## Name: generate_E
     ## Description: Generates the set of edges, E, to the graph G = (V,E).
     ## Argument(s): None
-    def generate_E(self): pass
-
+    def generate_E(self):
+        pass
+        
 if __name__ == "__main__":
-    pass
+    if len(sys.argv) < 3:
+        print(f"Invalid argument count: {len(sys.argv)} - Try again ...")
+        exit()
+    
+    v_count = int(sys.argv[1])                      ## Vertex Count
+    e_count = int(sys.argv[2])                      ## Edge Count
+    
+    if v_count < 0:                                 ## Check for valid |V|
+    print("Invaid vertex count, setting |V| = 1")
+    v_count = 1
+    
+    max_simple = (v_count * (v_count - 1)) // 2     ## Check for valid |E| / max_simple := C(|V|,2)
+    if e_count > max_simple:
+        print(f"Invalid edge count, setting |E| = {max_simple}")
+        e_count = max_simple
+    
+    ## Generating a random graph
+    G_rand = RandGraph(v_count, e_count)
