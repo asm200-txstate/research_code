52809f64f6ff1bbee32f32ee4dbd6de20b783649
axelsanchezmoreno <a_s1350@txstate.edu>
2025-05-27 23:09:02 -0500

    Updated driver function and added comments/descriptions to the functions

--- graph_visual/gen_graph.py
+++ graph_visual/gen_graph.py
@@ -10,12 +10,41 @@ import gurobipy as GRB
 from random import randint
 
 class GraphVisual:
-    def __init__(self): pass
-    def display(self): pass
+    ## Name: __init__ (Driver)
+    ## Description: Initializes the set of vertices and edges, denoted
+    ##              V and E, respectively, for the graph G = (V,E).
+    ## Argument(s):
+    ## * V: Set of vertices to G.
+    ## * E: Set of edges to G.
+    def __init__(self, V, E):
+        self.V = V
+        self.E = E
+
+    ## Name: display_graph
+    ## Description: Displays the randomized graph to the terminal.
+    ## Argument(s): None
+    def display(self):
+        pass
     
 class RandGraph:
-    def __init__(self): pass
+    ## Name: __init__ (Driver)
+    ## Description: Initializes the number of vertices and edges,
+    ##              V and E, respectively, to the graph G = (V,E).
+    ## Argument(s):
+    ## * card_v: The number of vertices to G = (V,E)
+    ## * card_e: The number of edges to G = (V,E)
+    def __init__(self, card_v, card_e):
+        self.card_v = card_v
+        self.card_e = card_e
+
+    ## Name: generate_V
+    ## Description: Generates the set of vertices, V, to the graph G = (V,E).
+    ## Argument(s): None
     def generate_V(self): pass
+    
+    ## Name: generate_E
+    ## Description: Generates the set of edges, E, to the graph G = (V,E).
+    ## Argument(s): None
     def generate_E(self): pass
 
 if __name__ == "__main__":
