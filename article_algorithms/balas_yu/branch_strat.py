5c64fd5f6b15e9b1b0654b7abfad6737a8b17b8d
axelsanchezmoreno <a_s1350@txstate.edu>
2025-05-28 19:01:17 -0500

    Created a random independent set

--- article_algorithms/balas_yu/branch_strat.py
+++ article_algorithms/balas_yu/branch_strat.py
@@ -8,7 +8,7 @@
 import sys
 import networkx as nx
 import matplotlib.pyplot as plt
-from random import randint
+from random import random
 
 # Step 1: Generate an arbitrary graph G = (V,E) and independent set S \subset V
 # Step 2: Find an independent set U \subset V such that \alpha(G(U)) \leq V
@@ -43,7 +43,7 @@ class RandGraph:
         
         for i in range(self.card_v):                                ## Generate edges to K_{card_v}
             for j in range(self.card_v):
-                add_edge = randint(0,1)                             ## Randomly decide whether an edge is added
+                add_edge = 1 if random() < 0.50 else 0              ## Randomly decide whether an edge is added
                 if i < j and add_edge: E.append([i,j])
         
         return E
@@ -75,6 +75,35 @@ class GraphVisual:
         plt.title(f"Induced Subgraph for $K_{{{len(self.V)}}}$")
         plt.show()
 
+class RandIndSet:
+    ## Name: __init__ (Driver)
+    ## Description: Initializes the set of vertices and edges, denoted
+    ##              V and E, respectively, for the graph G = (V,E).
+    ## Argument(s):
+    ## * V: Set of vertices to G.
+    ## * E: Set of edges to G.
+    ## Return(s): None
+    def __init__(self, V, E):
+        self.V = V
+        self.E = E
+        
+    ## Name: gen_ind_set
+    ## Description: Generates a random independent set U \subseteq S to G = (V,E).
+    ## Argument(s): None
+    ## Return(s): None
+    def gen_ind_set(self):
+        U = []                                                      ## Independent set U
+       
+        for v in self.V:
+            invalid = False
+            selected = 1 if random() < 0.5 else 0
+            if selected:                                            ## Check if a vertex is selected
+                for u in U:
+                    if [u,v] in E: invalid = True                   ## Check if there's a connection to vertices in U
+                if not invalid: U.append(v)
+        
+        return U
+    
 if __name__ == "__main__":
     if len(sys.argv) < 2:
         print(f"Invalid argument count: {len(sys.argv)} - Try again ...")
@@ -90,7 +119,10 @@ if __name__ == "__main__":
     V = G_rand.generate_V()
     E = G_rand.generate_E()
     
-    for e in E: print(e)
-
-#    G_disp = GraphVisual(V,E)
-#    G_disp.display()
+    IndSet = RandIndSet(V,E)
+    S = IndSet.gen_ind_set()                                        ## Random independent set S
+    
+#    for s in S: print(s)                                           ## Display the generated independent set
+    
+    G_disp = GraphVisual(V,E)
+    G_disp.display()
