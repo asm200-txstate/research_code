86d375ae76751aa361338cbc23cb818478124208
axelsanchezmoreno <a_s1350@txstate.edu>
2025-06-02 19:19:32 -0500

    Created mis_set and gen_Vi methods

--- article_algorithms/balas_yu/branch_strat.py
+++ article_algorithms/balas_yu/branch_strat.py
@@ -11,6 +11,7 @@ import matplotlib.pyplot as plt
 from random import random
 import gurobipy as gp
 from gurobipy import GRB
+from matplotlib.lines import Line2D
 
 # Step 1: Generate an arbitrary graph G = (V,E) and independent set S \subset V
 # Step 2: Find an independent set U \subset V such that \alpha(G(U)) \leq V
@@ -65,7 +66,15 @@ class GraphVisual:
         ## Edge Updates
         nx.draw_networkx_edges(G, pos, width=3, edgelist=Et, edge_color="tab:red", **extras)
         
-        plt.title(f"Test...")
+        ## Formats the legend in plot
+        legend_elements = [
+            Line2D([0], [0], marker='o', label='G = (V, E)', markerfacecolor='tab:blue'),
+            Line2D([0], [0], marker='o', label=r'$\tilde{G} = (U, \tilde{E})$', markerfacecolor='tab:red', color='tab:red')
+        ]
+
+        plt.legend(handles=legend_elements, loc='upper right')
+
+        plt.title(r"Induced Subgraph Plot")
         plt.show()
     
 class RandGraph:
@@ -140,27 +149,13 @@ class BBStrat:
     def __init__(self, V, E):
         self.V = V
         self.E = E
-    
-    ## Name: find_ind_set
-    ## Description: Find an independent set U \subseteq V such that \alpha(G[U]) <= |S|
-    ## Argument(s):
-    ## * U - A subset of vertices to the induced subgraph, G[U]
-    ## Return(s):
-    ## * Et - Denoted as \tilde{E}, the set of all edges to the induced subgraph
-    def edge_set(self, U):
-        Et = []
         
-        for e in self.E:
-            u, v = e
-            if u in U and v in U:
-                Et.append(e)                                        ## Append if both endpoints of e are in U
-        
-        return Et
-    
     ## Name: mis_cost
-    ## Description: Uses an IP to determine the maximum independent set to an induced subgraph, G[U], of G = (U,V)
-    ## Argument(s): None
-    ## Return(s): None
+    ## Description: Uses an IP to determine the optimal cost to the maximum independent set a graph G = (U, \tilde{E})
+    ## Argument(s):
+    ## * U - Set of vertices to G = (U, \tilde{E})
+    ## * Et - Set of edges to G = (U, \tilde{E})
+    ## Return(s): The optimal cost to the maximal independent set problem to G = (U, \tilde{E})
     def mis_cost(self, U, Et):
     
         model = gp.Model("mis_model")
@@ -194,22 +189,107 @@ class BBStrat:
         
         return opt_cost
         
-    ##
-    ##
-    ##
-    ##
-    def mis_set(self): pass
-    
-    ##
-    ##
-    ##
-    ##
-    def gen_vi(self, VnU, v):
-        Vi = []
+    ## Name: mis_set
+    ## Description: Uses an IP to determine the maximum independent set to an induced subgraph, G[U], of G = (U,V)
+    ## Argument(s):
+    ## * U - Set of vertices to G = (U, \tilde{E})
+    ## * Et - Set of edges to G = (U, \tilde{E})
+    ## Return(s): The set solution to the maximal independent set problem to G = (U, \tilde{E})
+    def mis_set(self, U, Et):
+        model = gp.Model("mis_model")
+        model.setParam("OutputFlag", 0)                             ## Prevent all comments from spamming terminal
         
-        Ni = []
-        for e in self.E: pass
+        x = model.addVars(U, vtype=GRB.BINARY, name="DV: x")
+        
+        ## Edge Constraints
+        for [u,v] in Et:
+            model.addConstr(x[u] + x[v] <= 1, f"C1 - Edge: ({u},{v})")
+            
+        ## Vertex Constraints
+        for u in U:
+            model.addConstr(x[u] >= 0, f"C2: {u}")
+
+        cost_coeff = []
+        for v in U:
+            cost_coeff.append(1)                                    ## Unweighted maximum independent set
+            
+        c = {v : cost_coeff[i] for i, v in enumerate(U)}
+
+        ## Objective Function: max c'x
+        objective = gp.quicksum(c[v] * x[v] for v in U)
+        model.setObjective(objective, gp.GRB.MAXIMIZE)
     
+        ## Perform optimization
+        model.optimize()
+        
+        # Display optimal solution
+        print("Optimal Solution: ")
+        for v in model.getVars():
+            print(f"{v.VarName}, {v.X:.0f}")
+        
+        Si = []
+        for soln, v in zip(x, model.getVars()):
+            if v.X == 1:
+                Si.append(soln)
+                print(f"soln: {soln}")
+        
+        return Si
+        
+    ## Name: gen_Vi
+    ## Description: Generates the set Vi := \overline{N}(xi) \ {xj : j < i}
+    ## Argument(s):
+    ## * x - Renamed as VnU, the list of entires in V but not in U
+    ## * i - Renamed as v, the ith vertex in VnU, used to construct Vi
+    ## Return(s): The constructed set Vi in step 3
+    def gen_Vi(self, x, i):
+        Vi = []
+    
+        Ni = []                                                 ## Neighborhood of v
+        for e in self.E:
+            u1, u2 = e
+            if x[i] in [u1, u2]:
+                if x[i] != u1: Ni.append(u1)
+                else: Ni.append(u2)
+        
+        print(f"x{i} = {x[i]} | N(x{i}):", Ni)
+        
+        ## Append vertices, generate \tilde{x} := {xj : j < i}
+        xj_tilde = []
+        for j in range(len(x)):
+            if j < i: xj_tilde.append(x[j])
+            
+        print("xj_tilde:", xj_tilde)
+        
+        NotNi = []
+        for u in self.V:                                        ## Non-neighborhood of v
+            if (u not in Ni) and (u != x[i]): NotNi.append(u)
+    
+        print(f"NotNi(x{j}):", NotNi)
+        
+        Vi = NotNi                                              ## Vertex Vi set
+        for v in xj_tilde:
+            if v in NotNi: Vi.remove(v)
+    
+        print(f"V{i}:", Vi)
+        
+        return Vi
+            
+    ## Name: gen_Ei
+    ## Description: Find the set of edges to a subset of vertices to V
+    ## Argument(s):
+    ## * U - A subset of vertices to the induced subgraph, G[U]
+    ## Return(s):
+    ## * Ei - Denoted as \tilde{E}, the set of all edges to the induced subgraph
+    def gen_Ei(self, U):
+        Ei = []
+        
+        for e in self.E:
+            u, v = e
+            if u in U and v in U:
+                Ei.append(e)                                        ## Append if both endpoints of e are in U
+        
+        return Ei
+
 if __name__ == "__main__":
     if len(sys.argv) < 2:
         print(f"Invalid argument count: {len(sys.argv)} - Try again ...")
@@ -220,50 +300,92 @@ if __name__ == "__main__":
         print("Invaid vertex count, setting |V| = 1")
         v_count = 1
     
-    ## Generating a random graph G = (V,E)
-    G_rand = RandGraph(v_count)
-    V = G_rand.generate_V()
-    E = G_rand.generate_E()
+#    ## Generating a random graph G = (V,E)
+#    G_rand = RandGraph(v_count)
+#    V = G_rand.generate_V()
+#    E = G_rand.generate_E()
+
+    ## Create a specific graph (temporarily)
+    V = [v+1 for v in range(10)]
+    E = [[1,2], [2,3], [3,4], [4,5],
+         [6,7], [7,8], [8,9], [9,10], [3,8]]
     
-    ## Generating a random independent set of vertices to G = (V,E)
-    Rand_IS = RandIndSet(V,E)
-    S = Rand_IS.gen_ind_set()                                       ## Generating a random independent set S
-            
+    G_disp = GraphVisual(V,E)
+    
+#    ## Generating a random independent set, S, of vertices to G = (V,E)
+#    Rand_IS = RandIndSet(V,E)
+#    S = Rand_IS.gen_ind_set()
+    S = [1,2,3]
+    
     ## Perform the Branch and Bound Strategy by Balas & Yu
     BB = BBStrat(V,E)
     
-    U = []
-    valid = False                                                   ## Flag to whether alpha(G[U]) <= |S|
-    while not valid:
-        ## Step 1a: Find a subset to V called U
-        U = []                                                      ## Reset after each iteration
-        for v in V:
-            if random() < 0.5: U.append(v)                          ## Select some vertices
-        
-        ## Step 1b: Find the edges to the induced subgraph, G[U]
-        Et = BB.edge_set(U)
-        
-        ## Step 1c: Find the maximal independent set to the induced subgraph of G[U] := (U, \tilde{E})
-        a_Gu = BB.mis_cost(U, Et)
-        
-        ## Step 1d: Check if alpha(G[U]) <= |S|
-        if a_Gu <= len(S): valid = True
-        
-#    print(f"alpha(G[U]) = {a_Gu}")
-#    print(f"|S| = {len(S)}")
+    ## Step 1: Find U \subseteq V where \alpha(G[U]) \leq |S|
+#    U = []
+#    valid = False                                                   ## Check if alpha(G[U]) <= |S|
+#    while not valid:
+#        ## Step 1a: Find a subset to V called U
+#        U = []                                                      ## Reset after each iteration
+#        for v in V:
+#            if random() < 0.5: U.append(v)                          ## Select some vertices
+#        
+#        ## Step 1b: Find the edges to the induced subgraph, G[U]
+#        Et = BB.gen_Ei(U)
+#        
+#        ## Step 1c: Find the maximal independent set to the induced subgraph of G[U] := (U, \tilde{E})
+#        a_Gu = BB.mis_cost(U, Et)
+#        
+#        ## Step 1d: Check if alpha(G[U]) <= |S|
+#        if a_Gu <= len(S): valid = True
+    U = [4,5,8,9]
+    Et = BB.gen_Ei(U)
+    
+    print("U: ", U)
     
     ## Step 2: Order vertices of V\U as x_{1}, ..., x_{k}
-    VnU = []
+    VnU, idx = [], 0
     for v in V:
-        if v not in U: VnU.append(v)                                ## Already ordered, so find vertices in V\U
-    
-    ## Step 3a: Generate Vi for each i where Vi := \overline{N}(xi)\{xj : j < i}
-    for v in VnU:
-        Vi = BB.gen_vi(VnU, v)
+        if v not in U:
+            VnU.append(v)
+            idx += 1
         
-        ## Step 3b: Find the edges to the induced subgraph with vertices Vi
-        
-        ## Step 3b: Find the maximal independent set, Si, to the induced subgraph
+    ## Step 3: For each i, let Vi := \overline{N}(xi) \ {xj : j < i}
+    ## and find a maximal independent set Si in G[Vi]
     
-    G_disp = GraphVisual(V,E)
-    G_disp.disp_ind_subgraph(U,Et)
+    print("\nVnU:", VnU, "\n")
+    
+    S_list = []
+    S_list.append(S)                                                 ## Append the first independent set
+    
+    idx = 0
+    for i in range(len(VnU)):
+        Vi = BB.gen_Vi(VnU,i)
+        Ei = BB.gen_Ei(Vi)
+        
+        Si = BB.mis_set(Vi,Ei)
+        print("Si: ", Si)
+        print("\n")
+
+        G_disp.disp_ind_subgraph(Vi,Ei)
+        
+        idx += 1
+  
+#    VnU = []
+#    for v in V:
+#        if v not in U: VnU.append(v)                                ## Already ordered, so find vertices in V\U
+#    print("VnU: ", VnU)
+#    
+#    G_disp.disp_ind_subgraph(U,Et)
+    
+#    count = 0
+#    ## Step 3a: Generate Vi for each i where Vi := \overline{N}(xi)\{xj : j < i}
+#    for v in VnU:
+#        Vi = BB.gen_vi(VnU, v)
+#        print(Vi)
+#
+#        ## Step 3b: Find the edges to the induced subgraph with vertices Vi
+#        
+#        ## Step 3b: Find the maximal independent set, Si, to the induced subgraph
+#    
+#    G_disp = GraphVisual(V,E)
+#    G_disp.disp_ind_subgraph(U,Et)
