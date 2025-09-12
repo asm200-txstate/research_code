9d83e562251c1e7c7198859b1b5fdb6aecc628f5
axelsanchezmoreno <a_s1350@txstate.edu>
2025-09-12 10:45:36 -0500

    Added new graphs and updated Babel method code

--- article_algorithms/comb_branch_bound/Babel/BabelMethod.py
+++ article_algorithms/comb_branch_bound/Babel/BabelMethod.py
@@ -5,151 +5,163 @@ import networkx as nx
 from BalasXue.WMISIP import WMISIP
 from BalasYu.RecSimpFix import RSF
 
-class BMethod:
+class BWCCMethod:
     def __init__(self, G : nx, W : dict): 
         self.graph = G
         self.weights = W
-        self.clique_list = []
-
-        self.S = []
-        self.S_weight = 0
-
-    def GKD(self):
-        gkd_dict = {}
+        self.clique_list, self.seen_list = [], []
+    
+    def compute_GKD(self):
+        self.clique_list = list(nx.find_cliques(self.graph))
+        gkd_vertex, gkd_weight = -1, -1
         for v in self.graph.nodes:
-            non_Nv = list(nx.non_neighbors(self.graph, v))
-        
-            K_list = []
-            for u in non_Nv:
-                for K in self.clique_list:
-                    if u in K and K not in K_list: K_list.append(list(K))
-
-            gkd_dict[v] = K_list
-
-        print(f"Clique list (so far): {self.clique_list}")
-
-        return gkd_dict
-
-    def babel_method(self):
-        seen = {v : False for v in self.graph.nodes}
-        for v in self.graph.nodes:
-            gkd_dict = self.GKD()                   # Re-define the GKD model - meant to find the vertex with the maximum GKD that hasn't been seen after every iteration.   
-
-            # Finding the vertices with the largest GKD  
-            max_dict, max_len = {}, -1
-            for key, val in gkd_dict.items():
-                print(f"Here at {key} with a list of length {len(val)}")
-                if len(val) > max_len and not seen[key]: 
-                    max_dict, max_len = {}, len(val)
-                    max_dict[key] = val
-                elif len(val) == max_len: 
-                    max_dict[key] = val
-
-            # Breaking ties with the heaviest vertex in the graph
-            max_list, max_vertex_weight, max_vertex = None, -1, -1
-            for key, val in max_dict.items():
-                if self.weights[key] > max_vertex_weight and not seen[key]: 
-                    max_list = val
-                    max_vertex = key
-                    max_vertex_weight = self.weights[key]
-
-            # Mark max_vertex as seen
-            seen[max_vertex] = True                
-            print(f"Vertex {max_vertex} was the optimal GKD - flagging as seen ...")
-
-            for clique in max_list: print(f"Clique in Max List: {clique}")
-
-            # Initializing W with the weight of max_vertex
-            W_value = self.weights[max_vertex]
-
-            print(f"Current Weight: {W_value}")
-
-            # Check every clique in the current clique list 
-            for clique in self.clique_list:
-                Nv = list(nx.neighbors(self.graph, max_vertex))
-
-                # If the clique is a subset of the neighborhood of max_vertex, decrement W_value and append max_vertex to the current clique
-                if set(clique).issubset(Nv):                        
-                    clique.append(max_vertex)
-                    W_value -= 1
-                if W_value == 0: break
-            
-            # Append singleton cliques of max_vertex W_value times
-            if W_value > 0: 
-                for _ in range(W_value): self.clique_list.append([max_vertex])
-
-        # print(f"Final clique list: {self.clique_list}")
-
-        # Check that the equivalent defintion of the weighted clique cover is satisfied. 
-        for v in self.graph.nodes:
-            # print(f"W({v}) = {self.weights[v]}")
             clique_count = 0
-            for clique in self.clique_list:
-                if v in clique: clique_count += 1
-            if self.weights[v] <= clique_count: 
-                print(f"Pass! We have |K : {v} in K| >= w({v})")
-            else: print(f"Error! We have |K : {v} in K| < w({v}) ...")
+            for u in nx.non_neighbors(self.graph, v):
+                for C in self.clique_list:
+                    if u in C: clique_count = clique_count + 1
+            if gkd_weight < clique_count and v not in self.seen_list:
+                gkd_vertex, gkd_weight = v, clique_count 
 
-        print("Final Clique List: ")
-        for clique in self.clique_list:
-            print(f"Clique {clique}")
+        self.seen_list.append(gkd_vertex)
+        return gkd_vertex
+    
+    def babel_scheme(self):
+        for _ in range(len(self.graph.nodes)):
+            gkd_v = self.compute_GKD()
 
-    def node_elimination(self): 
-        rsf_model = RSF()
-        S_hat = rsf_model.find_simplicial(self.graph)
-        S = rsf_model.indepSimplicial(self.graph, S_hat)
+            W = self.weights[gkd_v]
 
-        # print(f"S_hat output: {S_hat}")
-        # print(f"S output: {S}")
+            # Sort cliques from self.clique_list (review techniques later)
 
-        K_mathcal = {}
-        for v in self.graph.nodes:
-            non_Nv = list(nx.non_neighbors(self.graph, v))
-            non_Nv.append(v)
-            K_mathcal[v] = [] 
-            for u in non_Nv:
-                for clique in self.clique_list:
-                    if u in clique: K_mathcal[v].append(clique)
+            t = len(self.clique_list)
+            for idx in range(t):
+                if set(self.clique_list[idx]).issubset(list(nx.neighbors(self.graph, gkd_v))): 
+                    self.clique_list[idx].append(gkd_v)
+                    W = W - self.weights[gkd_v]
+                    if W <= 0: break
 
-        S_weight = 0
-        for v in S: S_weight += self.weights[v]
-        self.S, self.S_weight = S, S_weight
+            print(f"Current output: {W}")
 
-        print(f"S Output: {S} - Weight: {S_weight}")
+    # def babel_method(self):
+    #     seen = {v : False for v in self.graph.nodes}
+    #     for v in self.graph.nodes:
+    #         gkd_dict = self.GKD()                   # Re-define the GKD model - meant to find the vertex with the maximum GKD that hasn't been seen after every iteration.   
 
-        for v in self.graph.nodes:
-            clique_list = K_mathcal[v]
-            if len(clique_list) <= S_weight:
-                print(f"Here in if statement - Vertex {v} ...")
+    #         # Finding the vertices with the largest GKD  
+    #         max_dict, max_len = {}, -1
+    #         for key, val in gkd_dict.items():
+    #             print(f"Here at {key} with a list of length {len(val)}")
+    #             if len(val) > max_len and not seen[key]: 
+    #                 max_dict, max_len = {}, len(val)
+    #                 max_dict[key] = val
+    #             elif len(val) == max_len: 
+    #                 max_dict[key] = val
 
-                non_Nv = list(nx.non_neighbors(self.graph, v))
-                non_Nv.append(v)
-                mwis_model = WMISIP(nx.induced_subgraph(self.graph, non_Nv), self.weights)
-                mwis_model.optimize()
-                G_alpha = mwis_model.opt_cost()
+    #         # Breaking ties with the heaviest vertex in the graph
+    #         max_list, max_vertex_weight, max_vertex = None, -1, -1
+    #         for key, val in max_dict.items():
+    #             if self.weights[key] > max_vertex_weight and not seen[key]: 
+    #                 max_list = val
+    #                 max_vertex = key
+    #                 max_vertex_weight = self.weights[key]
 
-                if G_alpha <= S_weight: print("We hit this case!")
-                else: print("Something else ...")
+    #         # Mark max_vertex as seen
+    #         seen[max_vertex] = True                
+    #         print(f"Vertex {max_vertex} was the optimal GKD - flagging as seen ...")
 
-    def branching_scheme(self):
-        # curr_cliques = list(nx.find_cliques(self.graph))
-        # print(f"Cliques in G: {curr_cliques}")
+    #         for clique in max_list: print(f"Clique in Max List: {clique}")
 
-        r = {}
-        for v in self.graph.nodes:
-            max_idx = 0
-            for idx, clique in enumerate(self.clique_list):
-                if v in clique: max_idx = idx
-                r[v] = max_idx
-            print(f"Vertex {v} - Max index = {max_idx}")
+    #         # Initializing W with the weight of max_vertex
+    #         W_value = self.weights[max_vertex]
 
-        v_order = []
-        for entry in sorted(r.items(), key=lambda item: item[1]): v_order.append(entry[0])
+    #         print(f"Current Weight: {W_value}")
 
-        s_idx = len(v_order)-1
-        for idx in range(len(v_order)):
-            if v_order[idx] > self.S_weight:
-                s_idx = idx
+    #         # Check every clique in the current clique list 
+    #         for clique in self.clique_list:
+    #             Nv = list(nx.neighbors(self.graph, max_vertex))
 
-        print("Final output: ")
-        for v in range(s_idx+1, len(v_order)-1): print(f"Vertex: {v}")
+    #             # If the clique is a subset of the neighborhood of max_vertex, decrement W_value and append max_vertex to the current clique
+    #             if set(clique).issubset(Nv):                        
+    #                 clique.append(max_vertex)
+    #                 W_value -= 1
+    #             if W_value == 0: break
+            
+    #         # Append singleton cliques of max_vertex W_value times
+    #         if W_value > 0: 
+    #             for _ in range(W_value): self.clique_list.append([max_vertex])
+
+    #     # print(f"Final clique list: {self.clique_list}")
+
+    #     # Check that the equivalent defintion of the weighted clique cover is satisfied. 
+    #     for v in self.graph.nodes:
+    #         # print(f"W({v}) = {self.weights[v]}")
+    #         clique_count = 0
+    #         for clique in self.clique_list:
+    #             if v in clique: clique_count += 1
+    #         if self.weights[v] <= clique_count: 
+    #             print(f"Pass! We have |K : {v} in K| >= w({v})")
+    #         else: print(f"Error! We have |K : {v} in K| < w({v}) ...")
+
+    #     print("Final Clique List: ")
+    #     for clique in self.clique_list:
+    #         print(f"Clique {clique}")
+
+    # def node_elimination(self): 
+    #     rsf_model = RSF()
+    #     S_hat = rsf_model.find_simplicial(self.graph)
+    #     S = rsf_model.indepSimplicial(self.graph, S_hat)
+
+    #     # print(f"S_hat output: {S_hat}")
+    #     # print(f"S output: {S}")
+
+    #     K_mathcal = {}
+    #     for v in self.graph.nodes:
+    #         non_Nv = list(nx.non_neighbors(self.graph, v))
+    #         non_Nv.append(v)
+    #         K_mathcal[v] = [] 
+    #         for u in non_Nv:
+    #             for clique in self.clique_list:
+    #                 if u in clique: K_mathcal[v].append(clique)
+
+    #     S_weight = 0
+    #     for v in S: S_weight += self.weights[v]
+    #     self.S, self.S_weight = S, S_weight
+
+    #     print(f"S Output: {S} - Weight: {S_weight}")
+
+    #     for v in self.graph.nodes:
+    #         clique_list = K_mathcal[v]
+    #         if len(clique_list) <= S_weight:
+    #             print(f"Here in if statement - Vertex {v} ...")
+
+    #             non_Nv = list(nx.non_neighbors(self.graph, v))
+    #             non_Nv.append(v)
+    #             mwis_model = WMISIP(nx.induced_subgraph(self.graph, non_Nv), self.weights)
+    #             mwis_model.optimize()
+    #             G_alpha = mwis_model.opt_cost()
+
+    #             if G_alpha <= S_weight: print("We hit this case!")
+    #             else: print("Something else ...")
+
+    # def branching_scheme(self):
+    #     # curr_cliques = list(nx.find_cliques(self.graph))
+    #     # print(f"Cliques in G: {curr_cliques}")
+
+    #     r = {}
+    #     for v in self.graph.nodes:
+    #         max_idx = 0
+    #         for idx, clique in enumerate(self.clique_list):
+    #             if v in clique: max_idx = idx
+    #             r[v] = max_idx
+    #         print(f"Vertex {v} - Max index = {max_idx}")
+
+    #     v_order = []
+    #     for entry in sorted(r.items(), key=lambda item: item[1]): v_order.append(entry[0])
+
+    #     s_idx = len(v_order)-1
+    #     for idx in range(len(v_order)):
+    #         if v_order[idx] > self.S_weight:
+    #             s_idx = idx
+
+    #     print("Final output: ")
+    #     for v in range(s_idx+1, len(v_order)-1): print(f"Vertex: {v}")
--- article_algorithms/comb_branch_bound/main.py
+++ article_algorithms/comb_branch_bound/main.py
@@ -23,35 +23,21 @@ sys.setrecursionlimit(1000000)
 
 import json
 
-from Graph.GenGraph import GenGraph                                             ## Randomly generate a graph G = (V,E)
 from Graph.GraphPlot import GraphPlot                                           ## Plot the instance of G
 
-from BalasYu.RecSimpFix import RSF
-from BalasYu.CCIP import CCIP
 from BalasYu.BScheme import BYBScheme                                           ## Access the class to perform the branch and bound algorithm
+from BalasYu.RecSimpFix import RSF
 from BalasYu.MISIP import MISIP
-from BalasYu.ChordalMethod import CMethod
 
 from BalasXue.WBScheme import BXWBScheme                                        ## Access the class to perform the weighted branch and bound algorithm 
-from BalasXue.WGreedyMethod import WGMethod
 from BalasXue.WMISIP import WMISIP
-from BalasXue.WChordalMethod import WCMethod
 
-from Babel.BabelMethod import BMethod
-
-from SafeColor.gen_cliques import gen_cliques
-from SafeColor.dual_ccip import dual_ccip
-from SafeColor.wcc_heuristic import wcc_generator
-
-from Max_WC.max_weight_cover import max_wc
+from Babel.BabelMethod import BWCCMethod
 
 from networkx import erdos_renyi_graph
-from itertools import combinations
 import networkx as nx
-import random 
-
-from random import randint
 import numpy as np
+import random 
 
 def exec_rsf_alg(G):
     rsf_model = RSF()
@@ -69,28 +55,29 @@ def exec_balas_xue_alg(G, W):
     wmis_output = BXMethod.branch_scheme(G, W, 1)
     return wmis_output
 
-def exec_babel_alg():
+def exec_babel_alg(G, W):
+    BMethod = BWCCMethod(G, W) 
+    BMethod.babel_scheme()
     pass
 
 def main(argc, argv):
 
-    G = erdos_renyi_graph(30, 0.5)
+    G = erdos_renyi_graph(25, 0.5)
 
-    # Weighted branching scheme by Balas-Xue  
-    count, limit = 0, 500
+    # Un-weighted Branching Scheme by Balas-Yu
+    tol = 1e-8
+    count, limit = 0, 5
+
     while True:
-        W = {}
-        for v in G.nodes: W[v] = 1 # randint(1, 10) 
-    
         opt_mis = exec_balas_yu_alg(G)
-        total_weight = 0
+        total_sum = 0
         for v in opt_mis:
-            total_weight = total_weight + 1
+            total_sum = total_sum + 1
 
         mis_model = MISIP(G)
         mis_model.optimize()
 
-        if total_weight == mis_model.opt_cost(): 
+        if total_sum == mis_model.opt_cost(): 
             count = count + 1
             print(f"{count} of the {limit}: Success!")
         else: 
@@ -125,6 +112,66 @@ def main(argc, argv):
         else:
             print("B is not a subset of A - Expected \ Algorithm")
             print(f"Missing entries: {BnA}\n")
+        
+        symm_diff = list(set(A).symmetric_difference(B))
+        
+        print(f"Vertices: {G.nodes}")
+        print(f"Edges: {G.edges}")
+
+        GPlot = GraphPlot()
+        GPlot.disp_graph(G)
+
+    # Weighted Branching Scheme by Balas-Xue
+    tol = 1e-8
+    count, limit = 0, 5
+    while True:
+        W = {}
+        for v in G.nodes: W[v] = random.uniform(1.5, 5.5)
+        
+        opt_wmis = exec_balas_xue_alg(G, W)
+        total_weight = 0
+        for v in opt_wmis:
+            total_weight = total_weight + W[v]
+
+        wmis_model = WMISIP(G, W)
+        wmis_model.optimize()
+
+        A, B = set(wmis_model.opt_soln()), set(opt_wmis)
+        if A.issubset(B) and B.issubset(A): 
+            count = count + 1
+            print(f"{count} of the {limit}: Success!")
+        else: 
+            print(f"{count} of the {limit}: Fail! Terminating at an invalid output ...\n")
+            break
+        
+        if count == limit: 
+            print(f"\nAll {limit} cases have passed! Great job!\n")
+            break
+        
+    if count < limit:
+        print(f"Expected Output:  {wmis_model.opt_soln()}")
+        print(f"Branching Output: {opt_wmis}\n")
+
+        print(f"Expected Weight:  {wmis_model.opt_cost()}")
+        print(f"Branching Weight: {total_weight}\n")
+
+        A, B = opt_wmis, wmis_model.opt_soln()
+        AnB, BnA = [], []
+
+        for v in A:
+            if v not in B: AnB.append(v)
+
+        for v in B:
+            if v not in A: BnA.append(v)
+
+        if AnB == []: print("A is a subset of B\n")
+        else:
+            print("A is not a subset of B - Algorithm \ Expected")
+            print(f"Missing entries: {AnB}\n")
+        if BnA == []: print(print("B is a subset of A\n"))
+        else:
+            print("B is not a subset of A - Expected \ Algorithm")
+            print(f"Missing entries: {BnA}\n")
 
         symm_diff = list(set(A).symmetric_difference(B))
         
@@ -135,71 +182,10 @@ def main(argc, argv):
         GPlot = GraphPlot()
         GPlot.disp_weight_graph(G, W)
 
-    # AB_union = list(set(A).union(B))
-    # v_list = list(set(A).union(B))
-
-    # print(f"v_list: {v_list}")
-    # print(f"AB_union: {AB_union}")
-
-    # *****************************************************************
-
-    # F0, F1 = exec_rsf_alg(G)
-    # print(f"F0 Set - Length: {len(F0)}")
-    # print(f"F1 Set - Length: {len(F1)}\n")  
-
-    # isgraph = nx.induced_subgraph(G, list(set(G.nodes).difference(set(F1).union(F0))))
-    # mis_list = exec_balas_yu_alg(isgraph)
-
-    # for key, curr_mis_list in mis_dict.items():
-    #     GPlot = GraphPlot()
-    #     GPlot.vertex_labels_P2(G, F0, F1, curr_mis_list, key) 
-
-    # *****************************************************************
-
-    return
-
-    print("Without using the RSF Model ...\n")
-
-    BYMethod = BYBScheme()
-    status = BYMethod.branch_scheme(G, 1)
-
-    print("\nUsing the RSF Model ...\n")
-
-    rsf_model = RSF()
-    simp_list = rsf_model.find_simplicial(G)
-    F0, F1 = rsf_model.recursive_simplicial_fixing(G)
-    # print(f"O entries: {F0}")
-    # print(f"1 entries: {F1}")
-
-    print(f"Simplicial Vertices: {simp_list}\n")
-    print(f"Length |V|: {len(G.nodes)}")
-    print(f"Length |F0|: {len(F0)}")
-    print(f"Length |F1|: {len(F1)}")
-
-    node_list = set(list(G.nodes)).difference(set(F0).union(F1))
-    ISG = nx.induced_subgraph(G, node_list)
-
-    # G = nx.florentine_families_graph()
-    BYMethod = BYBScheme()
-    status = BYMethod.branch_scheme(ISG, 1)
-    # if not status: print("******************** Invalid output ...")
-    # else: print("******************** Good output!")
-
-    print(f"\nLength |V|: {len(ISG.nodes)}")
-    print(f"Length |F0|: {len(F0)}")
-    print(f"Length |F1|: {len(F1)}")
-    print(f"Vertices in F1: {F1}")
-
-    V_orig, V_update = list(G.nodes), list(G.nodes)
-    for v in V_orig:
-        if v in F0 or v in F1: V_update.remove(v)
-
-    mis_model = MISIP(G)
-    mis_model.optimize()
-    print(f"Optimal solution on original G: {mis_model.opt_cost()}")
-
-    GPlot = GraphPlot()
-    GPlot.disp_rsf_colors(G, F0, F1, V_update)
+    # Weighted Clique Cover by Babel 
+    W = {}
+    for v in G.nodes: W[v] = random.randint(5, 50)
+    exec_babel_alg(G, W) 
     
 if __name__ == "__main__":
     if len(sys.argv) < 2: 
@@ -208,6 +194,3 @@ if __name__ == "__main__":
         # exit()
     
     main(len(sys.argv), sys.argv)
-
-# Expected Output:  [0, 1, 2, 4, 5, 7, 9]
-# Branching Output: [0, 1, 2, 4, 5, 6, 7]
